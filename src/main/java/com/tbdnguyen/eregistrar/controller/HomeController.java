68270319fdb9b89e4cf6f628a31edcf0c7d90304
Duong Nguyen <duongntb94@gmail.com>
2021-07-02 13:37:53 -0500

    Init project

---
+++ src/main/java/com/tbdnguyen/eregistrar/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.tbdnguyen.eregistrar.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author duong at 7/2/21
+ * @project eRegistrar
+ */
+@Controller
+public class HomeController {
+    public String showHomePage() {
+        return "";
+    }
+}
